9e0d0f641797c7af38507f62bb5cb451082d7c86
vikki-phucnguyen <phuc.nguyen@vikki.vn>
2025-10-05 09:01:02 +0700

    add accessToken

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
 import {Viewer} from "resium";
+import * as Cesium from "cesium";
+
+Cesium.Ion.defaultAccessToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNWRjODYxMS1jYjMzLTQxNjAtOGVhNC1lN2IxMjBiYjYyOTEiLCJpZCI6MzQ3MDE0LCJpYXQiOjE3NTk1NDg5MTh9.1K80Xg1Oa0uAyf__JZG17T-NWTzW7Bcgtb-niLTFjwM"
 
 export default function App() {
     return (
